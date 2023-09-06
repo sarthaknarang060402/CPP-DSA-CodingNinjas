547d1880f225da8bd08b4f4862f55553af7ddf3a
Sarthak Narang <91564487+sarthaknarang060402@users.noreply.github.com>
2023-09-06 14:09:04 +0530

    Update Replace Character Recursively.cpp

--- Recursion 2/Replace Character Recursively.cpp
+++ Recursion 2/Replace Character Recursively.cpp
@@ -1,18 +1,7 @@
+#include<bits/stdc++.h>
 void replaceCharacter(char input[], char c1, char c2)
 {
-    /* Don't write main().
-     * Don't read input, it is passed as function argument.
-     * No need to print or return the output.
-     * Change in the given input string itself.
-     * Taking input and printing output is handled automatically.
-     */
-    if (input[0] == '\0')
-        return;
-    if (input[0] == c1)
-    {
-        input[0] = c2;
-        replaceCharacter(input + 1, c1, c2);
-    }
-    else
-        replaceCharacter(input + 1, c1, c2);
+    if(input[0]=='\0')return;
+    if(input[0]==c1)input[0]=c2;
+    replaceCharacter(input+1,c1,c2);
 }
