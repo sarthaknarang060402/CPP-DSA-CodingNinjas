9ada3bd222a894efa50fa4f0a581710ac95f227f
Sarthak Narang <91564487+sarthaknarang060402@users.noreply.github.com>
2023-09-07 20:27:09 +0530

    Update Return subsets sum to K.cpp

--- Recursion 2/Return subsets sum to K.cpp
+++ Recursion 2/Return subsets sum to K.cpp
@@ -1,31 +1,25 @@
-
-
 int subsetSumToK(int input[], int n, int output[][50], int k)
 {
-    // Write your code here
-    if (n == 0)
-    {
-        if (k == 0)
-        {
-            output[0][0] = 0;
-            return 1;
-        }
-        else
-            return 0;
-    }
-
-    int smallOutput1 = subsetSumToK(input + 1, n - 1, output, k);
-    int smallOutput2 = subsetSumToK(input + 1, n - 1, output + smallOutput1, k - input[0]);
-
-    int smallOutputSize = smallOutput1 + smallOutput2;
-    for (int i = smallOutput1; i < smallOutputSize; i++)
-    {
-        for (int j = output[i][0]; j > 0; j--)
-        {
-            output[i][j + 1] = output[i][j];
-        }
-        output[i][1] = input[0];
-        output[i][0]++;
-    }
-    return smallOutputSize;
+	if(n==0)
+	{
+		if(k==0)
+		{
+			output[0][0]=0;
+			return 1;
+		}
+		else return 0;
+	}
+	int nottake = subsetSumToK(input+1,n-1,output,k); // this will add to the output.. 
+	int take = subsetSumToK(input+1,n-1,output + nottake ,k - input[0]); 
+	int subsets = nottake + take ;
+	for(int i=nottake;i<subsets;i++)
+	{
+		for(int j=output[i][0];j>0;j--)
+		{
+			output[i][j+1]=output[i][j];
+		}
+		output[i][1]=input[0];
+		output[i][0]++;
+	}
+	return subsets;
 }
