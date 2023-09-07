64d2930924e537c7ef6b1c86b6d6d1d34fd00be7
Sarthak Narang <91564487+sarthaknarang060402@users.noreply.github.com>
2023-09-07 22:24:09 +0530

    Update Print all Codes - String.cpp

--- Recursion 2/Print all Codes - String.cpp
+++ Recursion 2/Print all Codes - String.cpp
@@ -1,35 +1,21 @@
 #include <string.h>
 using namespace std;
-void pac(string input, string output)
+
+void solve(string input,string output)
 {
-    if (input.size() == 0)
-    {
-        cout << output << endl;
+    if(input.empty()){
+        cout<<output<<endl;
         return;
     }
-    string mapping[] = {"", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-                        "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
-                        "v", "w", "x", "y", "z"};
-    string newout = output;
-    int number = (int)(input[0]) - 48;
-    if (number <= 26)
-    {
-        output = newout + mapping[number];
-        pac(input.substr(1), output);
-    }
-    if (input.size() != 1)
-    {
-        int number2 = ((int)input[0] - 48) * 10 + (int)(input[1]) - 48;
-        if (number2 <= 26)
-        {
-            output = newout + mapping[number2];
-            pac(input.substr(2), output);
-        }
-    }
-    return;
+    char c1 = input[0]-'0' + 'a' - 1;
+    int b = (input[0]-'0')*10 + input[1]-'0';
+    char c2 = b + 'a' -1;
+    solve(input.substr(1),output+c1);
+    if(b>=10&&b<=26)
+        solve(input.substr(2),output+c2);
+
 }
 void printAllPossibleCodes(string input)
 {
-    string output = "";
-    pac(input, output);
+    solve(input,"");
 }
