d76abc4d869072b11c12c608f9f683b1245f8340
Sarthak Narang <91564487+sarthaknarang060402@users.noreply.github.com>
2023-09-08 09:05:02 +0530

    Update Pair sum in array.cpp

--- Time and Space Complexity Analysis/Pair sum in array.cpp
+++ Time and Space Complexity Analysis/Pair sum in array.cpp
@@ -1,51 +1,34 @@
-/*Time Complexity : O(n *log(n)) Space Complexity
-    : O(1) where n is size of input Array
-    / List */
 int pairSum(int *arr, int n, int num)
 {
-    sort(arr, arr + n);
-    int startIndex = 0;
-    int endIndex = n - 1;
-    int numPairs = 0;
-    while (startIndex < endIndex)
-    {
-        if (arr[startIndex] + arr[endIndex] < num)
-        {
-            startIndex++;
-        }
-        else if (arr[startIndex] + arr[endIndex] > num)
-        {
-            endIndex--;
-        }
-        else
-        {
-            int elementAtStart = arr[startIndex];
-            int elementAtEnd = arr[endIndex];
-            if (elementAtStart == elementAtEnd)
-            {
-                int totalElementsFromStartToEnd = (endIndex - startIndex) + 1;
-                numPairs += (totalElementsFromStartToEnd *
-                             (totalElementsFromStartToEnd - 1) / 2);
-                return numPairs;
-            }
-            int tempStartIndex = startIndex + 1;
-            int tempEndIndex = endIndex - 1;
-            while (tempStartIndex <= tempEndIndex &&
-                   arr[tempStartIndex] == elementAtStart)
-            {
-                tempStartIndex += 1;
-            }
-            while (tempEndIndex >= tempStartIndex &&
-                   arr[tempEndIndex] == elementAtEnd)
-            {
-                tempEndIndex -= 1;
-            }
-            int totalElementsFromStart = (tempStartIndex - startIndex);
-            int totalElementsFromEnd = (endIndex - tempEndIndex);
-            numPairs += (totalElementsFromStart * totalElementsFromEnd);
-            startIndex = tempStartIndex;
-            endIndex = tempEndIndex;
-        }
-    }
-    return numPairs;
+	int count = 0;
+	sort(arr,arr+n);
+	for(int i=0,j=n-1;i<j;)
+	{
+		if(arr[i]+arr[j]<num)i++;
+		else if(arr[i]+arr[j]>num)j--;
+		else // sum = num
+		{
+			if(arr[i]==arr[j])// means all in between are also same .. total combinartions will be (n*n-1)/2
+			{
+				count += (j -i +1)*(j-i)/2;
+				break;
+			}
+			int lcount = 1;
+			while(i<j&&arr[i]==arr[i+1])
+			{
+				lcount++;
+				i++;
+			}
+			int rcount = 1;
+			while(i<j&&arr[j]==arr[j-1])
+			{
+				rcount++;
+				j--;
+			}
+			count+=lcount*rcount;
+			i++;
+            j--;
+		}
+	}
+	return count;
 }
