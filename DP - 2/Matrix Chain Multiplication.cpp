6e0ea6de17822c584a0a68ae806e741b5433dfa5
Sarthak Narang <91564487+sarthaknarang060402@users.noreply.github.com>
2023-09-17 21:28:59 +0530

    Update Matrix Chain Multiplication.cpp

--- DP - 2/Matrix Chain Multiplication.cpp
+++ DP - 2/Matrix Chain Multiplication.cpp
@@ -1,28 +1,44 @@
-#include <climits>
-#include <vector>
+#include<bits/stdc++.h>
+// int solve(int i,int j,vector<int>&arr1,vector<vector<int>> &dp)
+// {
+//     if(i==j)return 0;
+//     if(dp[i][j]!=-1)return dp[i][j];
+//     int mini = 1e9;
+//     for(int k = i;k<j;k++)
+//     {
+//         int operations = arr1[i-1]*arr1[k]*arr1[j] + solve(i,k,arr1,dp) + solve(k+1,j,arr1,dp);
+//         mini = min(mini,operations);
+//     }
+//     return dp[i][j]=mini;
+// }
 
-int helper(vector<vector<int>> &dp, int *arr, int start, int end)
+// int matrixChainMultiplication(int* arr, int n) {
+//     n=n+1;
+//     vector<int> arr1;
+//     for(int i=0;i<n;i++)arr1.push_back(arr[i]);
+
+//     vector<vector<int>> dp(n,vector<int>(n,-1));
+//     return solve(1,n-1,arr1,dp);
+// }
+
+int matrixChainMultiplication(int *arr,int n)
 {
-    if (end - start == 1)
-        return 0;
+    n++;
+    int dp[n][n];
+    for(int i=0;i<n;i++)dp[i][i]=0;
 
-    if (dp[start][end])
-        return dp[start][end];
-
-    int ans = INT_MAX;
-    for (int k = start + 1; k < end; k++)
+    for(int i=n-1;i>=1;i--)
     {
-        int temp = helper(dp, arr, start, k) + helper(dp, arr, k, end) +
-                   arr[start] * arr[k] * arr[end];
-        ans = min(ans, temp);
+        for(int j=i+1;j<n;j++)
+        {
+            int mini = 1e9;
+            for(int k = i;k<j;k++)
+            {
+                int operations = arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+                mini = min(mini,operations);
+            }
+            dp[i][j]=mini;
+        }
     }
-    dp[start][end] = ans;
-    return ans;
-}
-
-int matrixChainMultiplication(int *arr, int n)
-{
-    // Write your code here
-    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-    return helper(dp, arr, 0, n);
+    return dp[1][n-1];
 }
